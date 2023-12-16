dde34f9a73e7ca7927e2d7a5054d5c6fdfe94349
Aqif Khan <87937375+khanaqif@users.noreply.github.com>
2023-12-16 19:43:01 +0530

    Create acedemics.java
    created a java file

---
+++ acedemics.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    int i = 5;
+    i = i++; // i is now 6
+    i = ++i; // i is now 7
+    System.out.println(i); // prints 7
+  }
+}
